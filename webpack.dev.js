3e44f3a3b7c062ef3146babfa0fa31e7d0f270e7
tomohirokino <tomohiro.kino@d2cdot.co.jp>
2018-07-20 17:05:16 +0900

    webpackのmigrate対応

--- webpack.dev.js
+++ webpack.dev.js
@@ -3,10 +3,11 @@ const common = require('./webpack.common.js');
 const path = require('path');
 
 module.exports = merge(common, {
-    devtool: 'eval',
-    devServer: {
-      contentBase: path.join(__dirname, "dist"),
-      compress: true,
-      port: 9000
-    }
+  mode: "development",
+  devtool: 'eval',
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    compress: true,
+    port: 9000
+  }
 });
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,11 +1,10 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
-const webpack = require('webpack');
 
 module.exports = merge(common, {
+  mode: "production",
   plugins: [
-    new CleanWebpackPlugin(['dist']),
-    new webpack.optimize.UglifyJsPlugin(),
+    new CleanWebpackPlugin(['dist'])
   ],
 });
